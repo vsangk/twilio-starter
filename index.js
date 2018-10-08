896f4df58a36c9b479e4eb48189a93d40a481c7b
Vince Sangkhae <vsangk@gmail.com>
2018-10-07 21:22:30 -0700

    Root response

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const MessagingResponse = require("twilio").twiml.MessagingResponse;
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.json(null);
+});
+
 app.post("/sms", (req, res) => {
   const twiml = new MessagingResponse();
 
