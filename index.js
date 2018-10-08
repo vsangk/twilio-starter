c07a5a262f7d68aadadee1847703573377958c66
Vince Sangkhae <vsangk@gmail.com>
2018-10-07 21:01:13 -0700

    Make port compatible with heroku

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-const http = require('http');
-const express = require('express');
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const http = require("http");
+const express = require("express");
+const MessagingResponse = require("twilio").twiml.MessagingResponse;
 
 const app = express();
 
-app.post('/sms', (req, res) => {
+app.post("/sms", (req, res) => {
   const twiml = new MessagingResponse();
 
-  twiml.message('The Robots are coming! Head for the hills!');
+  twiml.message("The Robots are coming! Head for the hills!");
 
-  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.writeHead(200, { "Content-Type": "text/xml" });
   res.end(twiml.toString());
 });
 
-http.createServer(app).listen(1337, () => {
-  console.log('Express server listening on port 1337');
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 
+http.createServer(app).listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
+});
